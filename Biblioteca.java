abce8f9262e9f8f6696fbcb38cce67c02ec7562f
LuizFRBZ <luizfernandobezerra0@gmail.com>
2025-03-07 02:09:26 -0300

    Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -4,20 +4,20 @@ public class Biblioteca {
     private ArrayList<Usuario> usuarios;
     private ArrayList<Livro> livros;
 
-    public biblioteca() {
-        livro = new ArrayList<Livro>();
+    public Biblioteca() {
+        livros = new ArrayList<Livro>();
         usuarios = new ArrayList<Usuario>();
     }
 
-    public ArrayList<Usuario> getUSuarios() {
+    public ArrayList<Usuario> getUsuarios() {
         return usuarios;
     }
 
-    public void setUsuarios(Arraylist<Usuarios> usuarios) {
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
         this.usuarios = usuarios;
     }
 
-    public ArrayList<Livros> getLivros() {
+    public ArrayList<Livro> getLivros() {
         return livros;
     }
 
@@ -26,31 +26,30 @@ public class Biblioteca {
     }
 
     public void adicionarLivro(Livro livro) {
-        livro.add(livro);
-        System.out.println("Livro adicionando: " + livro.getTitulo());
+        livros.add(livro);  // Adiciona o livro na lista
+        System.out.println("Livro adicionado: " + livro.getTitulo());
     }
 
-    public void adicionarUsuarios(Usuario usuario) {
-        usuario.add(usuario);
-        System.out,println("Usuário cadastrado: " + usuario.getNome());
+    public void adicionarUsuario(Usuario usuario) {
+        usuarios.add(usuario);  // Adiciona o usuário na lista
+        System.out.println("Usuário cadastrado: " + usuario.getNome());
     }
 
     public Livro buscarLivro(String titulo) {
-        for (Livro livro: livros){
+        for (Livro livro : livros) {
             if (livro.getTitulo().equals(titulo)) {
-                return livro;
+                return livro;  // Retorna o livro se encontrado
             }
         }
-
-        return null;
+        return null;  // Retorna null se não encontrado
     }
 
     public Usuario buscarUsuario(String nome) {
-        for (Usuario usuario: usuarios) {
-            if (usuarios.getNome().equals(nome)) {
-                return usuarios;
+        for (Usuario usuario : usuarios) {
+            if (usuario.getNome().equals(nome)) {
+                return usuario;  // Retorna o usuário se encontrado
             }
         }
-        return null;
+        return null;  // Retorna null se não encontrado
     }
 }
